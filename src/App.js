a315d80f3bf286b79669f1e39dafe446171568aa
AbbasHaghayeghi <sa.haghayeghi@gmail.com>
2022-12-26 17:21:02 +0330

    Tailwind Added

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Vicolo App
-    </div>
+<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
+  <div>
+    <div class="text-xl font-medium text-black">ChitChat</div>
+    <p class="text-slate-500">You have a new message!</p>
+  </div>
+</div>
   );
 }
 

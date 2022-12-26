b392976500a1d977079abf6ad1400158aeb47acc
AbbasHaghayeghi <sa.haghayeghi@gmail.com>
2022-12-26 18:19:36 +0330

    Color Palette Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,21 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors :{
+        'primary': "#434343",
+        'secondary': {
+        '1':"#FF7A00",
+        '2':"#FF3D00"
+      },
+        'gray': {
+          '1':"#FAFAFA",
+          '2':"#F2F2F2",
+          '3':"#E4E4E4",
+          '4':"#C4C4C4"
+        },
+      }
+    },
   },
   plugins: [],
 }

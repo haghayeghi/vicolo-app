909eaa14580861c198d26d9917ff6f5e38fea036
AbbasHaghayeghi <sa.haghayeghi@gmail.com>
2022-12-28 10:34:59 +0330

    SVG Logos added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,13 @@
 import BGDiv from "./BGDiv";
-import BGText from "./BGTextClip";
+import whiteLogo from "../assets/white-logo.svg"
 
 function Header() {
   return (
     <div className="h-[96px]">
-      <BGDiv className="h-[96px] w-[260px]">
-        ddd
+      <BGDiv className=" h-[96px] w-[260px]">
+        <img className="m-auto pt-[30px] " src={whiteLogo} alt="Vicolo22"/>
       </BGDiv>
 
-      <BGText className="text-5xl font-black">
-        What is this my dear
-      </BGText>
     </div>
   )
 }
